2faa3eb89b7d5261980ca0a41a94e326d478bb37
raflyrhamadhan <57212062+raflyrhamadhan@users.noreply.github.com>
2019-11-23 22:04:02 +0700

    Add files via upload

---
+++ array4.m
@@ -0,0 +1,3 @@
+HOHO = ["Dia" "Enggak" ; "Aku" "Tapi" ; "Sayang" " :(" ];
+x = HOHO(2,1) + " " + HOHO(3,1) + " " + HOHO(1,1) + " "  + HOHO(2,2) + " " + HOHO(1,1) + " " + HOHO(1,2) + " " + HOHO(3,1) + " " + HOHO(2,1)+ " " + HOHO(3,2)
+disp(x) 
